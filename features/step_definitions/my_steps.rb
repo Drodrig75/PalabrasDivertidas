7bea524bf52c1cc4a6cfe84ef2da5f05f312e1d1
drodrig75 <danielrodriguezbernal@gmail.com>
2015-02-04 14:25:05 -0200

    merg v2

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -10,19 +10,19 @@ Then(/^nick "(.*?)"$/) do |arg1|
   last_response.body.should =~ /#{arg1}/m
 end
 
-Then(/^el nombre de la columna(\d+) es "(.*?)"$/) do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
-end
+#Then(/^el nombre de la columna(\d+) es "(.*?)"$/) do |arg1, arg2|
+#  pending # express the regexp above with the code you wish you had
+#end
 
-Then(/^el boton tiene el texto "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
+#Then(/^el boton tiene el texto "(.*?)"$/) do |arg1|
+#  pending # express the regexp above with the code you wish you had
+#end
 
-Given(/^que estoy en la pantalla inicial$/) do
-  visit '/'
-end
+#Given(/^que estoy en la pantalla inicial$/) do
+#  visit '/'
+#end
 
-Then(/^debo ver "(.*?)"$/) do |text|
-  last_response.body.should =~ /#{text}/m
-end
+#Then(/^debo ver "(.*?)"$/) do |text|
+#  last_response.body.should =~ /#{text}/m
+#end
 
